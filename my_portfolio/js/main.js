d929698dc2989254d33ffc73a99f53fc24a67f35
Claire Devvie Concepcion <clairedevvieconcepcion@gmail.com>
2023-08-26 09:01:39 +0800

    Update final my_portfolio

--- my_portfolio/js/main.js
+++ my_portfolio/js/main.js
@@ -49,16 +49,128 @@ skillsHeader.forEach((el) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 
+const tab = document.querySelectorAll("data-target"),
+  tabContents = document.querySelectorAll("data-content");
+
+tab.forEach((tab) => {
+  tab.addEventListener("click"),
+    () => {
+      const target = document.querySelectorG(tab.dataset.target);
+
+      tabContents.forEach((tabContents) => {
+        tabContent.classList.remove("qualifiction__active");
+      });
+      target.classList.add("qualification_active");
+
+      tab.forEach((tab) => {
+        tab.classList.remove("qualification__active");
+      });
+      tab.classList.add("qualification__active");
+    };
+});
+
 /*==================== SERVICES MODAL ====================*/
+
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll("services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtns, i) => {
+  modalBtns.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalViews) => {
+      modalViews.classList.remove("active-modal");
+    });
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
+let swiperPortfolio = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursos: true,
+  spaceBetween: 48,
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+/*=================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+const sections = document.querySelectorAll("section[id");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+/*================== CHANGE BACKGROUND HEADER ====================*/
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
 
-/*==================== DARK LIGHT THEME ====================*/
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*==================== DARK LIGHT THEME =========
+===========*/
